2b96c5dca3800fa683a73888f807223aa35dd24e
student <evelyns60@nycstudents.net>
2022-11-07 12:32:44 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String []args){
+
+        int sum = 0;
+        int x = 5;
+        for (int i = 0; i < 10 + x; i += 2) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+
+
+
+    }
+}
